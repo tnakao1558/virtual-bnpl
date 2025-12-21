fc0f1e9e27ca6e3a3f4216559f8cfce53c65de01
tnakao1558 <tnakao@netprotections.co.jp>
2025-12-21 22:58:26 +0900

    modify ledger_entry_modle

--- src/app/models/ledger_entry.rb
+++ src/app/models/ledger_entry.rb
@@ -2,10 +2,17 @@
 
 class LedgerEntry < ActiveRecord::Base
   self.table_name = 'ledger_entries'
+  
+  # Disable STI: type is a domain enum, not an inheritance discriminator
+  self.inheritance_column = :_type_disabled
 
   # アソシエーション
   belongs_to :user
-  belongs_to :transaction, optional: true
+  # transaction は ActiveRecord の予約メソッドと衝突するため、bnpl_transaction にリネーム
+  belongs_to :bnpl_transaction,
+             class_name: 'Transaction',
+             foreign_key: 'transaction_id',
+             optional: true
   belongs_to :invoice, optional: true
   belongs_to :payment, optional: true
 end
